7e2ce3b5497b3587d07cb2b99697d1c4c85c4a95
m0slah <muhammedslahqadir@gmail.com>
2023-07-09 14:01:33 +0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
-<template><h1>First Commit</h1></template>
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="../public/pinia-logo.svg" alt="pinia logo ">
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
 
 <script></script>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '../asset/main.css'
+
 createApp(App).mount('#app')
